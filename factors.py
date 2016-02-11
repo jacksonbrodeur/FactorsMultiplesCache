4c238209dceabd31b2fa81fe6cfbf284e63fa340
Jackson Brodeur <jacksonbrodeur@gmail.com>
2016-02-10 19:01:05 -0800

    implemented caching to a file

--- factors.py
+++ factors.py
@@ -1,13 +1,25 @@
-cache = {}
+import pickle
+factor_cache = {}
+
+def init_factor_cache(file_name):
+    global factor_cache
+    cache_file = open(file_name, "rb")
+    factor_cache = pickle.load(cache_file)
+    cache_file.close()
+
+def save_factor_cache(file_name):
+    cache_file = open(file_name, "wb")
+    pickle.dump(factor_cache, cache_file)
+    cache_file.close()
 
 def print_factors(num_list):
-    global cache
+    global factor_cache
     sorted_list = sorted(num_list)
     key = tuple(sorted_list)
-    if key in cache:
+    if key in factor_cache:
         # Already calculated this one
         # No need to do it again
-        print(cache[key])
+        print(factor_cache[key])
         return
     factor_list = {}
     for i in range(0, len(sorted_list)):
@@ -18,11 +30,19 @@ def print_factors(num_list):
         factor_list[sorted_list[i]] = factors
 
     print(factor_list)
-    cache[key] = factor_list
+    factor_cache[key] = factor_list
+
+def print_multiples(num_list):
+    pass
+
 
 if __name__ == "__main__":
+    init_factor_cache("factor_cache.in")
+
     nums = [10, 5, 2, 20]
     print_factors(nums)
     print_factors(nums)
     print_factors(nums)
+
+    save_factor_cache("factor_cache.in")
 
