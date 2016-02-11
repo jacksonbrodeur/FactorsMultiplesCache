88ed8d1b960e3df1a2734ba8d9459a14a579a9a3
Jackson Brodeur <jacksonbrodeur@gmail.com>
2016-02-10 18:16:07 -0800

    implemented caching across calls to print_factors

--- factors.py
+++ factors.py
@@ -1,6 +1,14 @@
+cache = {}
 
 def print_factors(num_list):
+    global cache
     sorted_list = sorted(num_list)
+    key = tuple(sorted_list)
+    if key in cache:
+        # Already calculated this one
+        # No need to do it again
+        print(cache[key])
+        return
     factor_list = {}
     for i in range(0, len(sorted_list)):
         factors = []
@@ -10,8 +18,11 @@ def print_factors(num_list):
         factor_list[sorted_list[i]] = factors
 
     print(factor_list)
+    cache[key] = factor_list
 
 if __name__ == "__main__":
     nums = [10, 5, 2, 20]
+    print_factors(nums)
+    print_factors(nums)
     print_factors(nums)
 
